c2ebd0e3981c96a7bb9b50d88b0d8bf4bc04e273
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2022-09-29 18:37:12 -0600

    finished string example

--- python_strings.py
+++ python_strings.py
@@ -50,20 +50,26 @@ current_year = 2022
 # birth_year_statement = my_first_name + my_last_name + 'was born in' + str(my_year_of_birth)
 # print(birth_year_statement)
 
-birth_year_statement = "{} {} was born in {}"
+birth_year_statement = "{} {} was born in {}."
 print(birth_year_statement.format(my_first_name, my_last_name, my_year_of_birth))
 
-current_year_statement = "{} {} was born in {}. {} enjoyed celebrating {}."
-print(current_year_statement.format(my_first_name,my_last_name,my_year_of_birth,my_first_name,current_year))
+current_year_statement = "{} {} enjoyed celebrating {}."
+print(current_year_statement.format(birth_year_statement.format(my_first_name,my_last_name,my_year_of_birth),my_first_name,current_year))
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
-
+escape_example = "{}\'s birth year is {}."
+print(escape_example.format(my_first_name,my_year_of_birth))
+escape_last_name = '\t{} {}'
+print(escape_last_name.format(my_last_name,my_year_of_birth))
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
 #       - first name and last name in lower case
 #       - length of last name
 #       - first name and last name all in upper case
+print(my_first_name.casefold(), my_last_name.casefold())
+print(len(my_last_name))
+print(my_first_name.upper(), my_last_name.upper())
